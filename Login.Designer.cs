c56b9c45d5f430d5398a25a5749faaa5215a9500
JNevile <j.neville657@mybvc.ca>
2024-11-26 11:28:41 -0700

    add buttons, labels and picturebox placeholder to login form

--- Login.Designer.cs
+++ Login.Designer.cs
@@ -28,12 +28,89 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Login";
+            label1 = new Label();
+            label2 = new Label();
+            btnSubmit = new Button();
+            txtUsername = new TextBox();
+            txtPassword = new TextBox();
+            pictureBox1 = new PictureBox();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(120, 333);
+            label1.Name = "label1";
+            label1.Size = new Size(95, 25);
+            label1.TabIndex = 0;
+            label1.Text = "Username:";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(120, 398);
+            label2.Name = "label2";
+            label2.Size = new Size(91, 25);
+            label2.TabIndex = 1;
+            label2.Text = "Password:";
+            // 
+            // btnSubmit
+            // 
+            btnSubmit.Location = new Point(209, 480);
+            btnSubmit.Name = "btnSubmit";
+            btnSubmit.Size = new Size(112, 34);
+            btnSubmit.TabIndex = 2;
+            btnSubmit.Text = "Submit";
+            btnSubmit.UseVisualStyleBackColor = true;
+            // 
+            // txtUsername
+            // 
+            txtUsername.Location = new Point(231, 333);
+            txtUsername.Name = "txtUsername";
+            txtUsername.Size = new Size(231, 31);
+            txtUsername.TabIndex = 3;
+            // 
+            // txtPassword
+            // 
+            txtPassword.Location = new Point(231, 398);
+            txtPassword.Name = "txtPassword";
+            txtPassword.Size = new Size(231, 31);
+            txtPassword.TabIndex = 4;
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.Location = new Point(189, 46);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(219, 227);
+            pictureBox1.TabIndex = 5;
+            pictureBox1.TabStop = false;
+            // 
+            // Login
+            // 
+            AutoScaleDimensions = new SizeF(10F, 25F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(578, 630);
+            Controls.Add(pictureBox1);
+            Controls.Add(txtPassword);
+            Controls.Add(txtUsername);
+            Controls.Add(btnSubmit);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Name = "Login";
+            Text = "Login";
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private Label label1;
+        private Label label2;
+        private Button btnSubmit;
+        private TextBox txtUsername;
+        private TextBox txtPassword;
+        private PictureBox pictureBox1;
     }
 }
