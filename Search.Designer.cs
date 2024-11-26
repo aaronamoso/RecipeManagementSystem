b6837a64dfa7be819ac9d8688407a2c4433ef852
JNevile <j.neville657@mybvc.ca>
2024-11-26 11:41:53 -0700

    add dietary restrictions, serving selector, and season of recipes to search filters form

--- Search.Designer.cs
+++ Search.Designer.cs
@@ -30,13 +30,29 @@
         {
             label1 = new Label();
             label2 = new Label();
-            listView1 = new ListView();
+            lvRecipes = new ListView();
             rbtnBreakfast = new RadioButton();
             rbtnLunch = new RadioButton();
             rbtnDinner = new RadioButton();
             btnSpecial = new RadioButton();
             rbtnDesserts = new RadioButton();
             rbtnSnacks = new RadioButton();
+            rbtnOneServing = new RadioButton();
+            rbtnFourServing = new RadioButton();
+            rbtnTwoServing = new RadioButton();
+            rbtnSixServing = new RadioButton();
+            rbtnGlutenFree = new RadioButton();
+            rbtnVegan = new RadioButton();
+            rbtnDairy = new RadioButton();
+            rbtnVegetarian = new RadioButton();
+            btnSearch = new Button();
+            label3 = new Label();
+            label4 = new Label();
+            rbtnFall = new RadioButton();
+            rbtnWinter = new RadioButton();
+            rbtnSummer = new RadioButton();
+            rbtnSpring = new RadioButton();
+            label5 = new Label();
             SuspendLayout();
             // 
             // label1
@@ -51,19 +67,19 @@
             // label2
             // 
             label2.AutoSize = true;
-            label2.Location = new Point(33, 242);
+            label2.Location = new Point(33, 318);
             label2.Name = "label2";
             label2.Size = new Size(96, 25);
             label2.TabIndex = 1;
             label2.Text = "All recipes:";
             // 
-            // listView1
+            // lvRecipes
             // 
-            listView1.Location = new Point(33, 281);
-            listView1.Name = "listView1";
-            listView1.Size = new Size(182, 146);
-            listView1.TabIndex = 2;
-            listView1.UseCompatibleStateImageBehavior = false;
+            lvRecipes.Location = new Point(33, 382);
+            lvRecipes.Name = "lvRecipes";
+            lvRecipes.Size = new Size(182, 146);
+            lvRecipes.TabIndex = 2;
+            lvRecipes.UseCompatibleStateImageBehavior = false;
             // 
             // rbtnBreakfast
             // 
@@ -131,18 +147,202 @@
             rbtnSnacks.Text = "Snacks";
             rbtnSnacks.UseVisualStyleBackColor = true;
             // 
+            // rbtnOneServing
+            // 
+            rbtnOneServing.AutoSize = true;
+            rbtnOneServing.Location = new Point(379, 71);
+            rbtnOneServing.Name = "rbtnOneServing";
+            rbtnOneServing.Size = new Size(109, 29);
+            rbtnOneServing.TabIndex = 9;
+            rbtnOneServing.TabStop = true;
+            rbtnOneServing.Text = "1 serving";
+            rbtnOneServing.UseVisualStyleBackColor = true;
+            // 
+            // rbtnFourServing
+            // 
+            rbtnFourServing.AutoSize = true;
+            rbtnFourServing.Location = new Point(379, 163);
+            rbtnFourServing.Name = "rbtnFourServing";
+            rbtnFourServing.Size = new Size(117, 29);
+            rbtnFourServing.TabIndex = 10;
+            rbtnFourServing.TabStop = true;
+            rbtnFourServing.Text = "4 servings";
+            rbtnFourServing.UseVisualStyleBackColor = true;
+            // 
+            // rbtnTwoServing
+            // 
+            rbtnTwoServing.AutoSize = true;
+            rbtnTwoServing.Location = new Point(379, 119);
+            rbtnTwoServing.Name = "rbtnTwoServing";
+            rbtnTwoServing.Size = new Size(117, 29);
+            rbtnTwoServing.TabIndex = 11;
+            rbtnTwoServing.TabStop = true;
+            rbtnTwoServing.Text = "2 servings";
+            rbtnTwoServing.UseVisualStyleBackColor = true;
+            // 
+            // rbtnSixServing
+            // 
+            rbtnSixServing.AutoSize = true;
+            rbtnSixServing.Location = new Point(375, 210);
+            rbtnSixServing.Name = "rbtnSixServing";
+            rbtnSixServing.Size = new Size(186, 29);
+            rbtnSixServing.TabIndex = 12;
+            rbtnSixServing.TabStop = true;
+            rbtnSixServing.Text = "6 or more servings";
+            rbtnSixServing.UseVisualStyleBackColor = true;
+            // 
+            // rbtnGlutenFree
+            // 
+            rbtnGlutenFree.AutoSize = true;
+            rbtnGlutenFree.Location = new Point(603, 77);
+            rbtnGlutenFree.Name = "rbtnGlutenFree";
+            rbtnGlutenFree.Size = new Size(123, 29);
+            rbtnGlutenFree.TabIndex = 13;
+            rbtnGlutenFree.TabStop = true;
+            rbtnGlutenFree.Text = "Gluten free";
+            rbtnGlutenFree.UseVisualStyleBackColor = true;
+            // 
+            // rbtnVegan
+            // 
+            rbtnVegan.AutoSize = true;
+            rbtnVegan.Location = new Point(604, 125);
+            rbtnVegan.Name = "rbtnVegan";
+            rbtnVegan.Size = new Size(86, 29);
+            rbtnVegan.TabIndex = 14;
+            rbtnVegan.TabStop = true;
+            rbtnVegan.Text = "Vegan";
+            rbtnVegan.UseVisualStyleBackColor = true;
+            // 
+            // rbtnDairy
+            // 
+            rbtnDairy.AutoSize = true;
+            rbtnDairy.Location = new Point(603, 172);
+            rbtnDairy.Name = "rbtnDairy";
+            rbtnDairy.Size = new Size(113, 29);
+            rbtnDairy.TabIndex = 15;
+            rbtnDairy.TabStop = true;
+            rbtnDairy.Text = "Dairy free";
+            rbtnDairy.UseVisualStyleBackColor = true;
+            // 
+            // rbtnVegetarian
+            // 
+            rbtnVegetarian.AutoSize = true;
+            rbtnVegetarian.Location = new Point(607, 221);
+            rbtnVegetarian.Name = "rbtnVegetarian";
+            rbtnVegetarian.Size = new Size(120, 29);
+            rbtnVegetarian.TabIndex = 16;
+            rbtnVegetarian.TabStop = true;
+            rbtnVegetarian.Text = "Vegetarian";
+            rbtnVegetarian.UseVisualStyleBackColor = true;
+            // 
+            // btnSearch
+            // 
+            btnSearch.Location = new Point(520, 566);
+            btnSearch.Name = "btnSearch";
+            btnSearch.Size = new Size(112, 34);
+            btnSearch.TabIndex = 17;
+            btnSearch.Text = "Search";
+            btnSearch.UseVisualStyleBackColor = true;
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(374, 31);
+            label3.Name = "label3";
+            label3.Size = new Size(173, 25);
+            label3.TabIndex = 18;
+            label3.Text = "Number of servings:";
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Location = new Point(604, 30);
+            label4.Name = "label4";
+            label4.Size = new Size(181, 25);
+            label4.TabIndex = 19;
+            label4.Text = "Dietary requirements:";
+            // 
+            // rbtnFall
+            // 
+            rbtnFall.AutoSize = true;
+            rbtnFall.Location = new Point(383, 336);
+            rbtnFall.Name = "rbtnFall";
+            rbtnFall.Size = new Size(62, 29);
+            rbtnFall.TabIndex = 20;
+            rbtnFall.TabStop = true;
+            rbtnFall.Text = "Fall";
+            rbtnFall.UseVisualStyleBackColor = true;
+            // 
+            // rbtnWinter
+            // 
+            rbtnWinter.AutoSize = true;
+            rbtnWinter.Location = new Point(387, 400);
+            rbtnWinter.Name = "rbtnWinter";
+            rbtnWinter.Size = new Size(89, 29);
+            rbtnWinter.TabIndex = 21;
+            rbtnWinter.TabStop = true;
+            rbtnWinter.Text = "Winter";
+            rbtnWinter.UseVisualStyleBackColor = true;
+            // 
+            // rbtnSummer
+            // 
+            rbtnSummer.AutoSize = true;
+            rbtnSummer.Location = new Point(602, 337);
+            rbtnSummer.Name = "rbtnSummer";
+            rbtnSummer.Size = new Size(104, 29);
+            rbtnSummer.TabIndex = 22;
+            rbtnSummer.TabStop = true;
+            rbtnSummer.Text = "Summer";
+            rbtnSummer.UseVisualStyleBackColor = true;
+            // 
+            // rbtnSpring
+            // 
+            rbtnSpring.AutoSize = true;
+            rbtnSpring.Location = new Point(598, 391);
+            rbtnSpring.Name = "rbtnSpring";
+            rbtnSpring.Size = new Size(89, 29);
+            rbtnSpring.TabIndex = 23;
+            rbtnSpring.TabStop = true;
+            rbtnSpring.Text = "Spring";
+            rbtnSpring.UseVisualStyleBackColor = true;
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Location = new Point(375, 291);
+            label5.Name = "label5";
+            label5.Size = new Size(160, 25);
+            label5.TabIndex = 24;
+            label5.Text = "Recipes by season:";
+            // 
             // Search
             // 
             AutoScaleDimensions = new SizeF(10F, 25F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(800, 450);
+            ClientSize = new Size(800, 632);
+            Controls.Add(label5);
+            Controls.Add(rbtnSpring);
+            Controls.Add(rbtnSummer);
+            Controls.Add(rbtnWinter);
+            Controls.Add(rbtnFall);
+            Controls.Add(label4);
+            Controls.Add(label3);
+            Controls.Add(btnSearch);
+            Controls.Add(rbtnVegetarian);
+            Controls.Add(rbtnDairy);
+            Controls.Add(rbtnVegan);
+            Controls.Add(rbtnGlutenFree);
+            Controls.Add(rbtnSixServing);
+            Controls.Add(rbtnTwoServing);
+            Controls.Add(rbtnFourServing);
+            Controls.Add(rbtnOneServing);
             Controls.Add(rbtnSnacks);
             Controls.Add(rbtnDesserts);
             Controls.Add(btnSpecial);
             Controls.Add(rbtnDinner);
             Controls.Add(rbtnLunch);
             Controls.Add(rbtnBreakfast);
-            Controls.Add(listView1);
+            Controls.Add(lvRecipes);
             Controls.Add(label2);
             Controls.Add(label1);
             Name = "Search";
@@ -155,12 +355,28 @@
 
         private Label label1;
         private Label label2;
-        private ListView listView1;
+        private ListView lvRecipes;
         private RadioButton rbtnBreakfast;
         private RadioButton rbtnLunch;
         private RadioButton rbtnDinner;
         private RadioButton btnSpecial;
         private RadioButton rbtnDesserts;
         private RadioButton rbtnSnacks;
+        private RadioButton rbtnOneServing;
+        private RadioButton rbtnFourServing;
+        private RadioButton rbtnTwoServing;
+        private RadioButton rbtnSixServing;
+        private RadioButton rbtnGlutenFree;
+        private RadioButton rbtnVegan;
+        private RadioButton rbtnDairy;
+        private RadioButton rbtnVegetarian;
+        private Button btnSearch;
+        private Label label3;
+        private Label label4;
+        private RadioButton rbtnFall;
+        private RadioButton rbtnWinter;
+        private RadioButton rbtnSummer;
+        private RadioButton rbtnSpring;
+        private Label label5;
     }
 }
