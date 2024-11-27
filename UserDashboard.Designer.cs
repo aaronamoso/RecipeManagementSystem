8c9fa18c907cfd8c1e1c61e09dca3f8d9b0bf759
JNevile <j.neville657@mybvc.ca>
2024-11-27 12:35:12 -0700

    updates to user dashboared, create code to navigate user to the search form when they click the button More Search Options on the userdashboard form

--- UserDashboard.Designer.cs
+++ UserDashboard.Designer.cs
@@ -28,9 +28,22 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(UserDashboard));
             label1 = new Label();
             label2 = new Label();
             lbRecipes = new ListBox();
+            pictureBox1 = new PictureBox();
+            pictureBox2 = new PictureBox();
+            btnAdvancedSearch = new Button();
+            textBox1 = new TextBox();
+            btnSearch = new Button();
+            label3 = new Label();
+            label4 = new Label();
+            label5 = new Label();
+            btnViewAllRecipes = new Button();
+            btnLogout = new Button();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox2).BeginInit();
             SuspendLayout();
             // 
             // label1
@@ -45,7 +58,7 @@
             // label2
             // 
             label2.AutoSize = true;
-            label2.Location = new Point(426, 71);
+            label2.Location = new Point(612, 80);
             label2.Name = "label2";
             label2.Size = new Size(124, 25);
             label2.TabIndex = 1;
@@ -55,21 +68,124 @@
             // 
             lbRecipes.FormattingEnabled = true;
             lbRecipes.ItemHeight = 25;
-            lbRecipes.Location = new Point(432, 123);
+            lbRecipes.Location = new Point(612, 125);
             lbRecipes.Name = "lbRecipes";
             lbRecipes.Size = new Size(222, 279);
             lbRecipes.TabIndex = 2;
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.Image = (Image)resources.GetObject("pictureBox1.Image");
+            pictureBox1.Location = new Point(89, 334);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(159, 214);
+            pictureBox1.SizeMode = PictureBoxSizeMode.StretchImage;
+            pictureBox1.TabIndex = 3;
+            pictureBox1.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            pictureBox2.Image = (Image)resources.GetObject("pictureBox2.Image");
+            pictureBox2.Location = new Point(333, 334);
+            pictureBox2.Name = "pictureBox2";
+            pictureBox2.Size = new Size(158, 214);
+            pictureBox2.SizeMode = PictureBoxSizeMode.StretchImage;
+            pictureBox2.TabIndex = 4;
+            pictureBox2.TabStop = false;
+            // 
+            // btnAdvancedSearch
+            // 
+            btnAdvancedSearch.Location = new Point(125, 150);
+            btnAdvancedSearch.Name = "btnAdvancedSearch";
+            btnAdvancedSearch.Size = new Size(235, 34);
+            btnAdvancedSearch.TabIndex = 5;
+            btnAdvancedSearch.Text = "More search options";
+            btnAdvancedSearch.UseVisualStyleBackColor = true;
+            btnAdvancedSearch.Click += btnAdvancedSearch_Click;
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(74, 93);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(253, 31);
+            textBox1.TabIndex = 6;
+            // 
+            // btnSearch
+            // 
+            btnSearch.Location = new Point(349, 90);
+            btnSearch.Name = "btnSearch";
+            btnSearch.Size = new Size(112, 34);
+            btnSearch.TabIndex = 7;
+            btnSearch.Text = "Search";
+            btnSearch.UseVisualStyleBackColor = true;
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(78, 252);
+            label3.Name = "label3";
+            label3.Size = new Size(159, 25);
+            label3.TabIndex = 8;
+            label3.Text = "New recipes to try:";
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Location = new Point(97, 306);
+            label4.Name = "label4";
+            label4.Size = new Size(140, 25);
+            label4.TabIndex = 9;
+            label4.Text = "Chicken tortillas:";
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Location = new Point(317, 306);
+            label5.Name = "label5";
+            label5.Size = new Size(187, 25);
+            label5.TabIndex = 10;
+            label5.Text = "Chocolate fudge cake:";
+            // 
+            // btnViewAllRecipes
+            // 
+            btnViewAllRecipes.Location = new Point(321, 252);
+            btnViewAllRecipes.Name = "btnViewAllRecipes";
+            btnViewAllRecipes.Size = new Size(183, 34);
+            btnViewAllRecipes.TabIndex = 11;
+            btnViewAllRecipes.Text = "See all recipes";
+            btnViewAllRecipes.UseVisualStyleBackColor = true;
+            // 
+            // btnLogout
+            // 
+            btnLogout.Location = new Point(665, 443);
+            btnLogout.Name = "btnLogout";
+            btnLogout.Size = new Size(112, 34);
+            btnLogout.TabIndex = 12;
+            btnLogout.Text = "Logout";
+            btnLogout.UseVisualStyleBackColor = true;
             // 
             // UserDashboard
             // 
             AutoScaleDimensions = new SizeF(10F, 25F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(800, 450);
+            ClientSize = new Size(977, 584);
+            Controls.Add(btnLogout);
+            Controls.Add(btnViewAllRecipes);
+            Controls.Add(label5);
+            Controls.Add(label4);
+            Controls.Add(label3);
+            Controls.Add(btnSearch);
+            Controls.Add(textBox1);
+            Controls.Add(btnAdvancedSearch);
+            Controls.Add(pictureBox2);
+            Controls.Add(pictureBox1);
             Controls.Add(lbRecipes);
             Controls.Add(label2);
             Controls.Add(label1);
             Name = "UserDashboard";
             Text = "UserDashboard";
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox2).EndInit();
             ResumeLayout(false);
             PerformLayout();
         }
@@ -79,5 +195,15 @@
         private Label label1;
         private Label label2;
         private ListBox lbRecipes;
+        private PictureBox pictureBox1;
+        private PictureBox pictureBox2;
+        private Button btnAdvancedSearch;
+        private TextBox textBox1;
+        private Button btnSearch;
+        private Label label3;
+        private Label label4;
+        private Label label5;
+        private Button btnViewAllRecipes;
+        private Button btnLogout;
     }
 }
--- UserDashboard.cs
+++ UserDashboard.cs
@@ -16,5 +16,12 @@ namespace RecipeManagementSystem
         {
             InitializeComponent();
         }
+
+        private void btnAdvancedSearch_Click(object sender, EventArgs e)
+        {
+            //Navigates the user to the advanced search filters page when they click the button: More search options
+            Search searchForm = new Search();
+            searchForm.Show(); // Displays the Search 
+        }
     }
 }
