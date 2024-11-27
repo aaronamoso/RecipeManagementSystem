1a0d3e8eab67a6c2c475f885d94904dfa722a155
JNevile <j.neville657@mybvc.ca>
2024-11-27 11:45:52 -0700

    start form design for userdashboard, add labels and listbox for saved recipes

--- UserDashboard.Designer.cs
+++ UserDashboard.Designer.cs
@@ -28,12 +28,56 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "UserDashboard";
+            label1 = new Label();
+            label2 = new Label();
+            lbRecipes = new ListBox();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(28, 25);
+            label1.Name = "label1";
+            label1.Size = new Size(58, 25);
+            label1.TabIndex = 0;
+            label1.Text = "Mealz";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(426, 71);
+            label2.Name = "label2";
+            label2.Size = new Size(124, 25);
+            label2.TabIndex = 1;
+            label2.Text = "Saved recipes:";
+            // 
+            // lbRecipes
+            // 
+            lbRecipes.FormattingEnabled = true;
+            lbRecipes.ItemHeight = 25;
+            lbRecipes.Location = new Point(432, 123);
+            lbRecipes.Name = "lbRecipes";
+            lbRecipes.Size = new Size(222, 279);
+            lbRecipes.TabIndex = 2;
+            // 
+            // UserDashboard
+            // 
+            AutoScaleDimensions = new SizeF(10F, 25F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(lbRecipes);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Name = "UserDashboard";
+            Text = "UserDashboard";
+            ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private Label label1;
+        private Label label2;
+        private ListBox lbRecipes;
     }
 }
