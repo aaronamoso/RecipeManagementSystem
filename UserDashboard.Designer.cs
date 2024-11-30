877a6cacabecee3744adeabf1e127b5eec6f2dab
JNevile <j.neville657@mybvc.ca>
2024-11-30 11:31:11 -0700

    userdashboard fonts labels design

--- UserDashboard.Designer.cs
+++ UserDashboard.Designer.cs
@@ -49,36 +49,41 @@
             // label1
             // 
             label1.AutoSize = true;
+            label1.Font = new Font("Cooper Black", 26F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label1.ForeColor = Color.SteelBlue;
             label1.Location = new Point(28, 25);
             label1.Name = "label1";
-            label1.Size = new Size(58, 25);
+            label1.Size = new Size(174, 60);
             label1.TabIndex = 0;
             label1.Text = "Mealz";
             // 
             // label2
             // 
             label2.AutoSize = true;
-            label2.Location = new Point(612, 80);
+            label2.Font = new Font("Cooper Black", 12F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label2.ForeColor = Color.SteelBlue;
+            label2.Location = new Point(591, 86);
             label2.Name = "label2";
-            label2.Size = new Size(124, 25);
+            label2.Size = new Size(186, 27);
             label2.TabIndex = 1;
             label2.Text = "Saved recipes:";
             // 
             // lbRecipes
             // 
+            lbRecipes.BackColor = Color.AliceBlue;
             lbRecipes.FormattingEnabled = true;
             lbRecipes.ItemHeight = 25;
             lbRecipes.Location = new Point(612, 125);
             lbRecipes.Name = "lbRecipes";
-            lbRecipes.Size = new Size(222, 279);
+            lbRecipes.Size = new Size(243, 329);
             lbRecipes.TabIndex = 2;
             // 
             // pictureBox1
             // 
             pictureBox1.Image = (Image)resources.GetObject("pictureBox1.Image");
-            pictureBox1.Location = new Point(89, 334);
+            pictureBox1.Location = new Point(89, 337);
             pictureBox1.Name = "pictureBox1";
-            pictureBox1.Size = new Size(159, 214);
+            pictureBox1.Size = new Size(173, 214);
             pictureBox1.SizeMode = PictureBoxSizeMode.StretchImage;
             pictureBox1.TabIndex = 3;
             pictureBox1.TabStop = false;
@@ -86,90 +91,109 @@
             // pictureBox2
             // 
             pictureBox2.Image = (Image)resources.GetObject("pictureBox2.Image");
-            pictureBox2.Location = new Point(333, 334);
+            pictureBox2.Location = new Point(331, 337);
             pictureBox2.Name = "pictureBox2";
-            pictureBox2.Size = new Size(158, 214);
+            pictureBox2.Size = new Size(182, 214);
             pictureBox2.SizeMode = PictureBoxSizeMode.StretchImage;
             pictureBox2.TabIndex = 4;
             pictureBox2.TabStop = false;
             // 
             // btnAdvancedSearch
             // 
-            btnAdvancedSearch.Location = new Point(125, 150);
+            btnAdvancedSearch.BackColor = Color.SteelBlue;
+            btnAdvancedSearch.Font = new Font("Cooper Black", 12F, FontStyle.Italic, GraphicsUnit.Point, 0);
+            btnAdvancedSearch.ForeColor = Color.AliceBlue;
+            btnAdvancedSearch.Location = new Point(28, 168);
             btnAdvancedSearch.Name = "btnAdvancedSearch";
-            btnAdvancedSearch.Size = new Size(235, 34);
+            btnAdvancedSearch.Size = new Size(301, 55);
             btnAdvancedSearch.TabIndex = 5;
             btnAdvancedSearch.Text = "More search options";
-            btnAdvancedSearch.UseVisualStyleBackColor = true;
+            btnAdvancedSearch.UseVisualStyleBackColor = false;
             btnAdvancedSearch.Click += btnAdvancedSearch_Click;
             // 
             // textBox1
             // 
-            textBox1.Location = new Point(74, 93);
+            textBox1.BackColor = Color.AliceBlue;
+            textBox1.Location = new Point(89, 104);
             textBox1.Name = "textBox1";
             textBox1.Size = new Size(253, 31);
             textBox1.TabIndex = 6;
             // 
             // btnSearch
             // 
-            btnSearch.Location = new Point(349, 90);
+            btnSearch.BackColor = Color.SteelBlue;
+            btnSearch.Font = new Font("Cooper Black", 12F, FontStyle.Italic, GraphicsUnit.Point, 0);
+            btnSearch.ForeColor = Color.AliceBlue;
+            btnSearch.Location = new Point(348, 95);
             btnSearch.Name = "btnSearch";
-            btnSearch.Size = new Size(112, 34);
+            btnSearch.Size = new Size(143, 46);
             btnSearch.TabIndex = 7;
             btnSearch.Text = "Search";
-            btnSearch.UseVisualStyleBackColor = true;
+            btnSearch.UseVisualStyleBackColor = false;
             // 
             // label3
             // 
             label3.AutoSize = true;
-            label3.Location = new Point(78, 252);
+            label3.Font = new Font("Cooper Black", 12F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label3.ForeColor = Color.SteelBlue;
+            label3.Location = new Point(40, 262);
             label3.Name = "label3";
-            label3.Size = new Size(159, 25);
+            label3.Size = new Size(246, 27);
             label3.TabIndex = 8;
             label3.Text = "New recipes to try:";
             // 
             // label4
             // 
             label4.AutoSize = true;
-            label4.Location = new Point(97, 306);
+            label4.Font = new Font("Elephant", 9.999999F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label4.ForeColor = Color.LightSlateGray;
+            label4.Location = new Point(75, 308);
             label4.Name = "label4";
-            label4.Size = new Size(140, 25);
+            label4.Size = new Size(187, 26);
             label4.TabIndex = 9;
             label4.Text = "Chicken tortillas:";
             // 
             // label5
             // 
             label5.AutoSize = true;
-            label5.Location = new Point(317, 306);
+            label5.Font = new Font("Elephant", 9.999999F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label5.ForeColor = Color.LightSlateGray;
+            label5.Location = new Point(321, 308);
             label5.Name = "label5";
-            label5.Size = new Size(187, 25);
+            label5.Size = new Size(170, 26);
             label5.TabIndex = 10;
-            label5.Text = "Chocolate fudge cake:";
+            label5.Text = "Chocolate cake:";
             // 
             // btnViewAllRecipes
             // 
-            btnViewAllRecipes.Location = new Point(321, 252);
+            btnViewAllRecipes.BackColor = Color.SteelBlue;
+            btnViewAllRecipes.Font = new Font("Cooper Black", 12F, FontStyle.Italic, GraphicsUnit.Point, 0);
+            btnViewAllRecipes.ForeColor = Color.AliceBlue;
+            btnViewAllRecipes.Location = new Point(348, 168);
             btnViewAllRecipes.Name = "btnViewAllRecipes";
-            btnViewAllRecipes.Size = new Size(183, 34);
+            btnViewAllRecipes.Size = new Size(232, 55);
             btnViewAllRecipes.TabIndex = 11;
             btnViewAllRecipes.Text = "See all recipes";
-            btnViewAllRecipes.UseVisualStyleBackColor = true;
+            btnViewAllRecipes.UseVisualStyleBackColor = false;
             // 
             // btnLogout
             // 
-            btnLogout.Location = new Point(665, 443);
+            btnLogout.BackColor = Color.SteelBlue;
+            btnLogout.Font = new Font("Cooper Black", 12F, FontStyle.Italic, GraphicsUnit.Point, 0);
+            btnLogout.ForeColor = Color.AliceBlue;
+            btnLogout.Location = new Point(637, 499);
             btnLogout.Name = "btnLogout";
-            btnLogout.Size = new Size(112, 34);
+            btnLogout.Size = new Size(153, 63);
             btnLogout.TabIndex = 12;
             btnLogout.Text = "Logout";
-            btnLogout.UseVisualStyleBackColor = true;
+            btnLogout.UseVisualStyleBackColor = false;
             btnLogout.Click += btnLogout_Click;
             // 
             // UserDashboard
             // 
             AutoScaleDimensions = new SizeF(10F, 25F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(977, 584);
+            ClientSize = new Size(896, 611);
             Controls.Add(btnLogout);
             Controls.Add(btnViewAllRecipes);
             Controls.Add(label5);
